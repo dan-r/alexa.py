8ed552c4e9109f254fdf4ca379d52956f7676a37
Daniel Raper <djr2468@gmail.com>
2018-04-14 20:58:10 +0100

    Shuffled about example.py

--- example.py
+++ example.py
@@ -3,13 +3,15 @@ al = alexa() # Instantiate the module
 
 def hello(text):
     return [text, True] # [speech response, end session?]
-    
+
+
+al.registerLaunch(lambda: hello("Welcome to Test Skill!"))
+al.registerIntent("testIntent", lambda: hello("testIntent running"))
+
 def lambda_handler(event, context):
-    al.registerLaunch(lambda: hello("Welcome to Test Skill!"))
-    al.registerIntent("testIntent", lambda: hello("testIntent running"))
-    
     return al.route(event)
     
+
 
 launchrequest = {'request':{'type':'LaunchRequest'}}
 print(lambda_handler(launchrequest, None))
