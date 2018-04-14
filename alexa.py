24010095185af3177d6ad178bc9a7022e27019f6
Daniel Raper <djr2468@gmail.com>
2018-04-14 20:53:16 +0100

    Fixed typo

--- alexa.py
+++ alexa.py
@@ -9,13 +9,17 @@ class alexa:
             if self.launch is None: # Raises an error if the function isnt set
                 raise Exception("No launch function set. Use alexa.registerLaunch(function)")
                 
-            return self.speak(self.launch()[0], endSession=False) # Endsession forced to False - as per Amazon Submission guidelines
+            runLaunch = self.launch()
+            if isinstance(runLaunch, list):
+                return self.speak(runLaunch[0], endSession=False) # Endsession forced to False - as per Amazon Submission guidelines
+            else:
+                return self.speak(runLaunch, endSession=False)
         
         chosenIntent = event['request']['intent']['name'] # Gets the intent name
         if chosenIntent in self.intents: # If we know this intent
             runIntent = self.intents[chosenIntent]() # Run the intent
             
-            if isInstance(e, list): # If a list is given then endSession depending on the second parameter (index 1)
+            if isinstance(runIntent, list): # If a list is given then endSession depending on the second parameter (index 1)
                 return self.speak(runIntent[0], endSession=runIntent[1]) # Return the speech
             else: # If a string is returned, end the session after responding by default
                 return self.speak(runIntent, endSession=True)
